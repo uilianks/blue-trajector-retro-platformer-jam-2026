6c03bab8a5a83a64c8ac66c7dd13da9aa3018219
uilianks <uilianks@gmail.com>
2026-03-13 18:24:41 +0000

    first commit

---
+++ coin/coin.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	animated_sprite_2d.play("idle")
+	body_entered.connect(_on_body_entered)
+	animated_sprite_2d.animation_finished.connect(_on_animation_finished)
+
+func _on_body_entered(body: Node) -> void:
+	if body is CharacterBody2D:
+		set_deferred("monitoring", false)
+		animated_sprite_2d.play("picked")
+
+func _on_animation_finished() -> void:
+	if animated_sprite_2d.animation == "picked":
+		queue_free()
---
+++ common/signals/bus_signals.gd
@@ -0,0 +1,5 @@
+extends Node
+
+signal power_changed(value: float)
+signal angle_changed(value: float)
+signal ui_visible(power: bool, angle: bool)
