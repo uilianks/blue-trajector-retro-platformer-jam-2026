fe43ebc904426234e3fb83279bf0db63403e3204
uilianks <uilianks@gmail.com>
2026-03-15 02:20:22 +0000

    firt commit

--- common/life_manager/life_manager.gd
+++ common/life_manager/life_manager.gd
@@ -1,5 +1,5 @@
 extends Node
 
-const MAX_LIVES: int = 15
+const MAX_LIVES: int = 99999
 var current_lives: int = MAX_LIVES
 var life_potion_collected: bool = false
--- stages/main_menu/main_menu.gd
+++ stages/main_menu/main_menu.gd
@@ -5,4 +5,6 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://stages/credits/credits.tscn")
-	pass
+
+func _on_button_3_pressed() -> void:
+	get_tree().quit()
